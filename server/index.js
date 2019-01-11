c77e0d651a89c7c0d84d09e3c525a3fb8cfe782b
Your Name <wamontgomery0068@gmail.com>
2019-01-10 19:04:23 -0600

    Server is  live

--- server/index.js
+++ server/index.js
@@ -6,3 +6,22 @@ const session = require('express-session');
 const bcrypt = require("bcryptjs");
 const app = express();
 // const authController = require("./controllers/authController");
+
+app.use(json());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}))
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set("db", db);
+    console.log("Database Connected!")
+});
+
+app.listen(process.env.EXPRESS_PORT || 3056, () => {
+    console.log(`Listening on ${process.env.EXPRESS_PORT}`);
+});
