747b0cbf45f274ee439f8ca37b439e3210617c60
Your Name <wamontgomery0068@gmail.com>
2019-01-11 20:28:34 -0600

    Updating SQL

--- db/pizzeria/createPizzeria.sql
+++ db/pizzeria/createPizzeria.sql
@@ -14,3 +14,6 @@ twitter_url,
 instagram_url
 )
 VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
+
+INSERT INTO pizzeria (name, address, zipcode, city, state, cuisine, aggregate_rating, website_url, image_url, logo_url, facebook_url, twitter_url, instagram_url   )
+VALUES ('','',,'','','',,'','','','','','');
