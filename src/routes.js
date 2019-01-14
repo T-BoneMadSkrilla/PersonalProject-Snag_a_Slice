cd69100dc974b6ee607aad744c09075dd63644b7
Your Name <wamontgomery0068@gmail.com>
2019-01-14 09:56:55 -0600

    Setting up Routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-// import Dashboard from "./components/Views/Dashboard/Dashboard";
+import Home from "./components/Views/Home/Home";
 import Landing from "./components/Views/Landing/Landing";
 
 
 export default (
     <Switch>
         <Route path = "/" exact component = {Landing} />
-        {/* <Route path = "/home" component = {Dashboard} /> */}
+        <Route path = "/home" component = {Home} />
     </Switch>
 );
