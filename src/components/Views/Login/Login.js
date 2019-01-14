34e69c44c6e8ca9fca03f1dea41e55a99657dcbe
Your Name <wamontgomery0068@gmail.com>
2019-01-14 15:24:30 -0600

    Styling Complete now work on functionality

--- src/components/Views/Login/Login.js
+++ src/components/Views/Login/Login.js
@@ -4,8 +4,20 @@ import './Login.css'
 class Login extends Component {
     render(){
         return(
-            <div className = "Login_Container">
-                <h1> Login Component</h1>
+            <div className = "Login">
+                <div className = "Login_Container">
+                    <div className = "Pizza_Image_Login">
+                    <img class = "Navigation_Pizza" src="https://image.flaticon.com/icons/svg/1404/1404945.svg" alt="Pizza Pie" title = "Welcome" height="80" width="80" />
+                    </div>
+                    <div className = "Name_Login">
+                        <p className = "Name_Text_Login"> Snag A Slice </p>
+                    </div>
+                    <input className = "Text_Username_Login" type = "text" placeholder = "Enter Username" />
+                    <input className = "Text_Password_Login" type = "text" placeholder = "Enter Password" />
+                    <button className = "Submit_Login">
+                        <p className = "Submit_Text_Login"> Submit </p>
+                    </button>
+                </div>
             </div>
         )
     };
