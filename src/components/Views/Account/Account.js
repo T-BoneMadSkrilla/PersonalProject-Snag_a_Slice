dda161ef59cb6de78a48d3a7a0f894d31559bc16
Your Name <wamontgomery0068@gmail.com>
2019-01-15 09:18:39 -0600

    Changed name from information to account

---
+++ src/components/Views/Account/Account.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Account.css'
+
+class Account extends Component {
+    render(){
+        return(
+            <div className = "Account_Container">
+                <h1> Account Component</h1>
+            </div>
+        )
+    };
+};
+
+export default Account;
