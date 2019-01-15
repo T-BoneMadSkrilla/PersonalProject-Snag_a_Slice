e6040db7d6864c203ff72af08bab28fcf1a4cdd5
Your Name <wamontgomery0068@gmail.com>
2019-01-15 09:19:35 -0600

    Just doing route things

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+import {Provider} from 'react-redux';
 import Landing from "./components/Views/Landing/Landing";
 import Home from "./components/Views/Home/Home";
 import Pizzerias from "./components/Views/Pizzerias/Pizzerias";
-import Information from "./components/Views/Information/Information";
+import Account from "./components/Views/Account/Account";
 import Signup from "./components/Views/Signup/Signup";
 import Login from "./components/Views/Login/Login";
 import About from "./components/Views/About/About";
+import store from './store';
 
 
 export default (
-    <Switch>
-        <Route path = "/" exact component = {Landing} />
-        <Route path = "/home" component = {Home} />
-        <Route path = "/pizzerias" component = {Pizzerias} />
-        <Route path = "/information" component = {Information} />
-        <Route path = "/signup" component = {Signup} />
-        <Route path = "/login" component = {Login} />
-        <Route path = "/about" component = {About} />
-    </Switch>
+    <Provider store = {store}>
+        <Switch>
+            <Route path = "/" exact component = {Landing} />
+            <Route path = "/home" component = {Home} />
+            <Route path = "/pizzerias" component = {Pizzerias} />
+            <Route path = "/account" component = {Account} />
+            <Route path = "/signup" component = {Signup} />
+            <Route path = "/login" component = {Login} />
+            <Route path = "/about" component = {About} />
+        </Switch>
+    </Provider>
 );
