6b6287a384df7166609fd5aefd146fb96c6cbdd5
Your Name <wamontgomery0068@gmail.com>
2019-01-16 16:38:09 -0600

    Created New SQL

---
+++ db/addPizzeria.sql
@@ -0,0 +1,2 @@
+INSERT INTO pizzeria (name, address, zipcode, city, state, aggregate_rating, website_url, phone_number, logo_image, pizzeria_image, pizza_image )
+VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);
