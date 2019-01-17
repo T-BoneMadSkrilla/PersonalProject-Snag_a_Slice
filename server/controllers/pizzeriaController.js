c601df06f61054b25e316e06ff5650c8a4be0874
Your Name <wamontgomery0068@gmail.com>
2019-01-16 20:35:31 -0600

    Set up a controller for pizzerias

--- server/controllers/pizzeriaController.js
+++ server/controllers/pizzeriaController.js
@@ -1,6 +1,6 @@
 function getPizzerias(req, res){
     const db = req.app.get('db')
-    db.get_pizzerias()
+    db.get_pizzeria()
     .then(pizzerias => res.json(pizzerias))
     .catch(err => console.log(err))
 }
