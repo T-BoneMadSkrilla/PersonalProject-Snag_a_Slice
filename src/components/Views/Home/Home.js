67687b1fbe3d82e3d192895d4987c45e731547d0
Your Name <wamontgomery0068@gmail.com>
2019-01-18 20:56:20 -0600

    In the process of putting in Media Query

--- src/components/Views/Home/Home.js
+++ src/components/Views/Home/Home.js
@@ -7,22 +7,6 @@ class Home extends Component {
     render(){
         return(
             <div className = "Home_Container">
-                <div className = "Header_Container">
-                    <div className = "EditorsChoice_Container">
-                        <p className = "EditorsChoice_Text"> Editors Choice </p>
-                        <p className = "EditorsChoice_Pizzeria_Text"> Lupi's Pizza Pie </p>
-                        <p className = "EditorsChoice_Pizzeria_Location_Text"> Chattanooga, Tennessee </p>
-                        <a className = "EditorsChoice_Pizzeria_ViewInformation_Button" href = "https://www.lupi.com/" >
-                            <p className = "View_Information_Text"> View Website </p>
-                        </a>
-                    </div>
-                    <Link to = "/pizzerias">
-                    <div className = "Explore_Pizzerias_Container">
-                        <p className = "Explore_Pizzeria_Text"> Explore Pizzerias </p>
-                        <img class = "Explore_Pizza" src="pizza-slice.png" alt="Pizza Pie" title = "Welcome" height="20" width="20" />
-                    </div>
-                    </Link>
-                </div>
             </div>
         )
     }
