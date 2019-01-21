c2c86b21900800a6ff4b89bb989ce21da981ff1a
Your Name <wamontgomery0068@gmail.com>
2019-01-21 15:15:31 -0600

    Functionality Complete minor changes needed

---
+++ src/components/Bars/Hamburger/Hamburger.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './Hamburger.scss';
+
+class Hamburger extends Component {
+    state = {
+        sideNav: false,
+        links: ['home', 'about', 'pizzerias', 'signup', 'login']
+    };
+
+    render(){
+        const { sideNav, links} = this.state;
+        
+        const linkMap = links.map((e,i) => {
+            return (
+                <div className = "sideNav" key = {i}>
+                    <Link to = {`/${e}`}><p className = "sideNav_Text">{e}</p></Link>
+                </div>
+            );
+        });
+        return(
+            <div className = "Hamburger_Container">
+                <div className = "Hamburger_Bar">
+                    <button className = "Hamburger_Bar_Button" onClick={()=> this.setState({ sideNav: !sideNav})}>
+                        <img className = "Hamburger_Icon" src = "https://image.flaticon.com/icons/svg/56/56763.svg" />
+                    </button>
+                    <p className = "Hamburger_Text"> Menu </p>
+                </div>
+                <div className = "Hamburger_DropDown">
+                    <Link to = "/home">
+                    <p className = "Hamburger_Link_Text"> Home </p>
+                    </Link>
+                    <Link to = "/about">
+                    <p className = "Hamburger_Link_Text"> About </p>
+                    </Link>
+                    <Link to = "/pizzerias">
+                    <p className = "Hamburger_Link_Text"> Pizzerias </p>
+                    </Link>
+                    <Link to = "/signup">
+                    <p className = "Hamburger_Link_Text"> Sign up </p>
+                    </Link>
+                    <Link to = "/login">
+                    <p className = "Hamburger_Link_Text"> Login </p>
+                    </Link>
+                </div>
+                {sideNav && <div className="sideNav">{linkMap}</div>}
+            </div>
+        )
+        
+    }
+    
+};
+
+export default Hamburger;
