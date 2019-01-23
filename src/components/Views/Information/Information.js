85677e13edf0d36da4e35a72544b62a5022f79b9
Your Name <wamontgomery0068@gmail.com>
2019-01-23 12:09:59 -0600

    styling changes

--- src/components/Views/Information/Information.js
+++ src/components/Views/Information/Information.js
@@ -9,12 +9,20 @@ class Information extends Component {
     constructor(){
         super()
 
-        this.state = {}
+        this.state = {
+            userInput: ''
+        };
     };
 
     componentDidMount(){
         this.props.getPizzeria()
     };
+
+    // User Section
+
+    addComment(){}
+
+    deleteComment(){}
 
     render(){
         // console.log(this.props.user[+this.props.match.params.id])
@@ -40,14 +48,34 @@ class Information extends Component {
                                 <p className = "Information_Container_Top_Bottom_Text"> City: {this.props.user[+this.props.match.params.id].city} </p>
                                 <p className = "Information_Container_Top_Bottom_Text"> State: {this.props.user[+this.props.match.params.id].state} </p>
                             </div>
-                            <div className = "Information_Container_Top_Bottom_Middle"></div>
                             <div className = "Information_Container_Top_Bottom_Right">
                                 <p className = "Information_Container_Top_Bottom_Bottom_Text" > Phone Number: {this.props.user[+this.props.match.params.id].phone_number} </p>
                                 <p className = "Information_Container_Top_Bottom_Bottom_Text" > Website: {this.props.user[+this.props.match.params.id].website_url} </p>
+                                <p className = "Information_Container_Top_Bottom_Bottom_Text" > Add Comment Below </p>
+                                <p className = "Information_Container_Top_Bottom_Bottom_Text" > View Pizzeria Reviews Below  </p>
                             </div>
                         </div>
                     </div>
-                    <div className = "Information_Container_Bottom"></div>
+                    <div className = "Information_Container_Bottom">
+                        <div className = "Information_Container_Bottom_Top">
+                            <div className = "Information_Container_Bottom_Top_Title">
+                                <p className = "Information_Container_Bottom_Top_Title_Text"> Add a Comment: {this.props.user[+this.props.match.params.id].name} </p>
+                            </div>
+                            <input className = "Information_Container_Bottom_Top_Input_Field" placeholder = "Type Here ...."></input>
+                            <div className = "Information_Container_Bottom_Top_Post_Delete">
+                                <button className = "Information_Container_Bottom_Top_Post_Delete_Button"> 
+                                    <p className = "Information_Container_Bottom_Top_Post_Button_Text"> Post </p>
+                                </button>
+                                <button className = "Information_Container_Bottom_Top_Post_Delete_Button"> 
+                                    <p className = "Information_Container_Bottom_Top_Delete_Button_Text"> Delete </p>
+                                </button>
+                            </div>
+                            <div className = "Information_Container_Bottom_Top_Input_Field_Result">
+                                <p className = "Information_Container_Bottom_Top_Input_Field_Result_Text" > {this.props.user[+this.props.match.params.id].pizzeria_review} </p>
+                            </div>
+                        </div>
+                        <div className = "Information_Container_Bottom_Bottom"></div>                        
+                    </div>
                 </div>
             </div>
         )
