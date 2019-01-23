7fd5fbc33d340272a42acecdad70b25f085935e7
Your Name <wamontgomery0068@gmail.com>
2019-01-23 12:06:01 -0600

    Did not need the table

--- db/get_pizzeria_city.sql
+++
@@ -1,7 +0,0 @@
--- This will return all the Pizzerias in a specific city from the database.
-
-SELECT * FROM pizzeria WHERE city = 'Nashvile';
-
-SELECT * FROM pizzeria WHERE city = 'Atlanta';
-
-SELECT * FROM pizzeria WHERE city = 'New Orleans';
--- db/get_pizzeria_state.sql
+++
@@ -1,7 +0,0 @@
--- This will return all the Pizzerias in a specific city from the database.
-
-SELECT * FROM pizzeria WHERE state = 'Tennessee';
-
-SELECT * FROM pizzeria WHERE state = 'Georgia';
-
-SELECT * FROM pizzeria WHERE state = 'Texas';
