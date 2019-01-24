cfb36cb99ae3799f83f53885259551787d558836
Your Name <wamontgomery0068@gmail.com>
2019-01-24 11:25:58 -0600

    Changed

--- server/controllers/pizzeriaController.js
+++ server/controllers/pizzeriaController.js
@@ -9,8 +9,38 @@ function getPizzerias(req, res){
 
 // Pizzeria Review Functionality
 
+function getComments(req, res){
+    const db = req.app.get('db')
+    db.get_Pizzeria_Comments()
+    .then(response => res.json(response))
+    .catch(error_in_backend => console.log(error_in_backend))
+};
 
+function postComments(req, res){
+    const db = req.app.get('db')
+    db.addComments([req.body.id, req.body.review, req.body.pizza_id])
+    .then(response => res.json(response))
+    .catch(error_in_backend => console.log(error_in_backend))
+}
+
+function updateComments(req, res){
+    const db = req.app.get('db')
+    db.updateComment([req.body.id, req.body.review, req.body.pizza_id])
+    .then(response => res.json(response))
+    .catch(error_in_backend => console.log(error_in_backend))
+};
+
+function deleteComments(req, res){
+    const db = req.app.get('db')
+    db.deleteComment([req.body.id])
+    .then(response => res.json(response))
+    .catch(error_in_backend => console.log(error_in_backend))
+};
 
 module.exports = {
     getPizzerias,
+    getComments,
+    postComments,
+    updateComments,
+    deleteComments
 }
--- src/components/Views/Information/Information.js
+++ src/components/Views/Information/Information.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from "react-router-dom";
 import {getPizzeria} from '../../../ducks/user';
+import {getComment} from '../../../ducks/user';
 
 import './Information.css'
 
@@ -10,19 +11,25 @@ class Information extends Component {
         super()
 
         this.state = {
-            userInput: ''
+            username: "",
+            review: "",
         };
     };
 
     componentDidMount(){
         this.props.getPizzeria()
+        this.props.getComment()
+
+    };
+    
+    handleChange(e){
+        this.setState({ [e.target.name]: e.target.value})
     };
 
-    // User Section
-
-    addComment(){}
-
-    deleteComment(){}
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.post(this.state.username, this.state.review, this.props.match.params.id)
+    };
 
     render(){
         // console.log(this.props.user[+this.props.match.params.id])
@@ -32,46 +39,124 @@ class Information extends Component {
                     <div className = "Information_Container_Top">
                         <div className = "Information_Container_Top_Top">
                             <div className = "Information_Container_Top_Top_Title">
-                                <p className = "Information_Container_Top_Top_Title_Text"> {this.props.user[+this.props.match.params.id].name} </p>
+                            
+                                <p 
+                                    className = "Information_Container_Top_Top_Title_Text"> 
+                                        {this.props.user[+this.props.match.params.id].name} 
+                                </p>
+
                             </div>
                             <div className = "Information_Container_Top_Top_Content">
-                                <img src = {this.props.user[+this.props.match.params.id].pizzeria_image}className = "Information_Container_Top_Top_Image" />
+
+                                <img 
+                                    src = {this.props.user[+this.props.match.params.id].pizzeria_image}className = "Information_Container_Top_Top_Image" 
+                                />
+
                                 <div className = "Information_Container_Top_Top_Rating">
-                                    <p className = "Information_Container_Top_Top_Rating_Text"> {this.props.user[+this.props.match.params.id].aggregate_rating} </p>
+
+                                    <p 
+                                        className = "Information_Container_Top_Top_Rating_Text"> 
+                                            {this.props.user[+this.props.match.params.id].aggregate_rating} 
+                                    </p>
+
                                 </div>
                             </div>
                         </div>
                         <div className = "Information_Container_Top_Bottom">
                             <div className = "Information_Container_Top_Bottom_Left">
-                                <p className = "Information_Container_Top_Bottom_Text"> Address: {this.props.user[+this.props.match.params.id].address} </p>
-                                <p className = "Information_Container_Top_Bottom_Text"> Zipcode: {this.props.user[+this.props.match.params.id].zipcode} </p>
-                                <p className = "Information_Container_Top_Bottom_Text"> City: {this.props.user[+this.props.match.params.id].city} </p>
-                                <p className = "Information_Container_Top_Bottom_Text"> State: {this.props.user[+this.props.match.params.id].state} </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Text"> 
+                                        Address: {this.props.user[+this.props.match.params.id].address} 
+                                </p>
+                                
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Text"> 
+                                        Zipcode: {this.props.user[+this.props.match.params.id].zipcode} 
+                                </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Text"> 
+                                        City: {this.props.user[+this.props.match.params.id].city} 
+                                </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Text"> 
+                                        State: {this.props.user[+this.props.match.params.id].state} 
+                                </p>
+
                             </div>
                             <div className = "Information_Container_Top_Bottom_Right">
-                                <p className = "Information_Container_Top_Bottom_Bottom_Text" > Phone Number: {this.props.user[+this.props.match.params.id].phone_number} </p>
-                                <p className = "Information_Container_Top_Bottom_Bottom_Text" > Website: {this.props.user[+this.props.match.params.id].website_url} </p>
-                                <p className = "Information_Container_Top_Bottom_Bottom_Text" > Add Comment Below </p>
-                                <p className = "Information_Container_Top_Bottom_Bottom_Text" > View Pizzeria Reviews Below  </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Bottom_Text" > 
+                                        Phone Number: {this.props.user[+this.props.match.params.id].phone_number} 
+                                </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Bottom_Text" > 
+                                        Website: {this.props.user[+this.props.match.params.id].website_url} 
+                                </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Bottom_Text" > 
+                                        Add Comment Below 
+                                </p>
+
+                                <p 
+                                    className = "Information_Container_Top_Bottom_Bottom_Text" > 
+                                        View Pizzeria Reviews Below  
+                                </p>
+
                             </div>
                         </div>
                     </div>
                     <div className = "Information_Container_Bottom">
                         <div className = "Information_Container_Bottom_Top">
                             <div className = "Information_Container_Bottom_Top_Title">
-                                <p className = "Information_Container_Bottom_Top_Title_Text"> Add a Comment: {this.props.user[+this.props.match.params.id].name} </p>
+
+                                <p 
+                                    className = "Information_Container_Bottom_Top_Title_Text">
+                                         Add a Comment: {this.props.user[+this.props.match.params.id].name} 
+                                </p>
+
                             </div>
-                            <input className = "Information_Container_Bottom_Top_Input_Field" placeholder = "Type Here ...."></input>
+
+                            <input 
+                                onChange = {this.handleChange}
+                                value = {this.state.review} 
+                                type = "text"
+                                name = "Users Review"
+                                placeholder = "Type Here ...."
+                                className = "Information_Container_Bottom_Top_Input_Field" 
+                            />
+
                             <div className = "Information_Container_Bottom_Top_Post_Delete">
-                                <button className = "Information_Container_Bottom_Top_Post_Delete_Button"> 
-                                    <p className = "Information_Container_Bottom_Top_Post_Button_Text"> Post </p>
+                                
+                                <button 
+                                    className = "Information_Container_Bottom_Top_Post_Delete_Button" 
+                                    title = "Edit your Comment" 
+                                    onClick >
+                                        <p className = "Information_Container_Bottom_Top_Edit_Button_Text"> Edit </p>
                                 </button>
-                                <button className = "Information_Container_Bottom_Top_Post_Delete_Button"> 
-                                    <p className = "Information_Container_Bottom_Top_Delete_Button_Text"> Delete </p>
+                                
+                                <button 
+                                    className = "Information_Container_Bottom_Top_Post_Delete_Button" 
+                                    title = "Post your Comment" 
+                                    onClick > 
+                                        <p className = "Information_Container_Bottom_Top_Post_Button_Text"> Post </p>
                                 </button>
+                                
+                                <button 
+                                    className = "Information_Container_Bottom_Top_Post_Delete_Button" 
+                                    title = "Delete your Comment" 
+                                    onClick > 
+                                        <p className = "Information_Container_Bottom_Top_Delete_Button_Text"> Delete </p>
+                                </button>
+
                             </div>
                             <div className = "Information_Container_Bottom_Top_Input_Field_Result">
-                                <p className = "Information_Container_Bottom_Top_Input_Field_Result_Text" > {this.props.user[+this.props.match.params.id].pizzeria_review} </p>
+                                <p className = "Information_Container_Bottom_Top_Input_Field_Result_Text" > {this.props.user[+this.props.match.params.id].review} </p>
                             </div>
                         </div>
                         <div className = "Information_Container_Bottom_Bottom"></div>                        
@@ -88,4 +173,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps, {getPizzeria: getPizzeria}) (Information);
+export default connect(mapStateToProps, {getPizzeria: getPizzeria, getComment: getComment}) (Information);
